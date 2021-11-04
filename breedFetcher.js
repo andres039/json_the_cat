18eb60323883389c4aedbbfa3f339101fae80c6b
Andres <andres039039@Hotmail.com>
2021-11-04 20:05:33 +0000

    Integrated into one function

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,16 +1,21 @@
 "use strict";
 const request = require("request");
 //https://api.thecatapi.com/v1/breeds/search?q=Siberian
-let catUrl = "https://api.thecatapi.com/v1/breeds/search?q=";
-const usersCat = process.argv[2];
-request(catUrl + usersCat, (error, response, body) => {
-  if (error) {
-    console.log(error);
-    return;
-  }
-  if (body.length === 2) {
-    console.log("we don't have that flavour!");
-  } else {
-    console.log(JSON.parse(body));
-  }
-});
+//const breedName = process.argv[2];
+const fetchBreedDescription = function(breedName, callback) {
+  let catUrl = "https://api.thecatapi.com/v1/breeds/search?q=";
+  request(catUrl + breedName, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    if (body.length === 2) {
+      callback("we don't have that flavour!", null);
+      return;
+      // console.log("we don't have that flavour!");
+    }
+    callback(null, JSON.parse(body)[0].description);
+    //console.log(JSON.parse(body));
+  });
+};
+module.exports = { fetchBreedDescription };
